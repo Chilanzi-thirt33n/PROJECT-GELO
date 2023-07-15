d696b7913a0a07fa4bff9bb790353c5709668bf3
chilanzi <davidchilanzijilliamdark@gmail.com>
2023-07-15 07:02:45 +0200

    added the gallery and some css aswell as js

---
+++ javascript/lightbox.js
@@ -0,0 +1,18 @@
+
+// ******   THIS IS A SMALL SCRIPT FOR LIGHT BOX FUNCTION
+// Get the modal by id
+var modal = document.getElementById("modal");
+
+// Get the modal image tag
+var modalImg = document.getElementById("modal-img");
+
+// this function is called when a small image is clicked
+function showModal(src) {
+  modal.classList.remove("hidden");
+  modalImg.src = src;
+}
+
+// this function is called when the close button is clicked
+function closeModal() {
+  modal.classList.add("hidden");
+}
